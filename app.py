e874ad5e17e7a6364cc9681025eeb1191a5d1f0b
Eduardo <latinzero1@hotmail.com>
2026-07-09 18:05:11 -0400

    feat: agrega Kelvin al menu de opciones

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import streamlit as st
 st.title("Conversor de temperatura entre Celsius y Fahrenheit")
 
 modo = st.radio("Convertir de:", [
-                "Celsius a Fahrenheit", "Fahrenheit a Celsius"])
+                "Celsius a Fahrenheit",
+                "Fahrenheit a Celsius",
+                "Celsius a Kelvin",
+                "Kelvin a Celsius"])
+
 valor = st.number_input("Valor")
 
 if modo == "Celsius a Fahrenheit":
