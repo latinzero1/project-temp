56bf8dcba2d7751a578b04963d340ef61901b305
Eduardo <latinzero1@hotmail.com>
2026-07-09 17:59:28 -0400

    feat: incluir logica de conversion a Kelvin

--- app.py
+++ app.py
@@ -9,6 +9,12 @@ valor = st.number_input("Valor")
 if modo == "Celsius a Fahrenheit":
     resultado = valor * 9 / 5 + 32
     st.write(f"{valor} °C son {round(resultado, 2)} °F")
-else:
+elif modo == "Fahrenheit a Celsius":
     resultado = (valor - 32) * 5 / 9
     st.write(f"{valor} °F son {round(resultado, 2)} °C")
+elif modo == "Celsius a Kelvin":
+    resultado = valor + 273.15
+    st.write(f"{valor} °C son {round(resultado, 2)} K")
+elif modo == "Kelvin a Celsius":
+    resultado = valor - 273.15
+    st.write(f"{valor} K son {round(resultado, 2)} °C")
